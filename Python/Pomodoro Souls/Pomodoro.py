c587d5f57d2abe471968c9397f3c6a42de85239b
Douglas Adones <contato.douglas_ad@outlook.com>
2022-01-20 07:50:05 -0300

    Atualizando Pomodoro

--- Python/Pomodoro Souls/Pomodoro.py
+++ Python/Pomodoro Souls/Pomodoro.py
@@ -2,15 +2,15 @@ from defpomodoro import *
 from time import sleep
 
 # Programa Principal
-print('*' * 53)
-print(f'{"POMODORO SOULS":^53}')
-print('*' * 53)
+print("*" * 53)
+print(f"{'POMODORO SOULS':^53}")
+print("*" * 53)
 
 # Solicitação de informações + Sistema Anti-Erro
 while True:
     ativo = descanso = ciclos = -1
-    loop = predefinido = imag = ' '
-    manter_parametros = 'S'
+    loop = predefinido = imag = " "
+    manter_parametros = "S"
     print(f"""{'Pomodoros Pré-definidos':^53}
 [1] - Curto: 25min ativo + 05min descanso, 2 Ciclos
       Tempo Total: 1h
@@ -29,12 +29,13 @@ while True:
     elif predefinido == "2":
         ativo, descanso, ciclos = 25, 5, 4
     else:
-        ativo = tratando_erros_int(ativo, "Informe o Tempo ativo (em minutos): ")
-        descanso = tratando_erros_int(descanso, "Informe o Tempo de descanso (em minutos):  ")
-        ciclos = tratando_erros_int(ciclos, "Quantos ciclos? ")
-    imag = tratando_erros_str(imag, "Deseja ativar os alertas de imagem? [S/N] ")
+        ativo = tratando_erros_int("Informe o Tempo ativo (em minutos): ")
+        descanso = tratando_erros_int("Informe o Tempo de descanso (em minutos):  ")
+        ciclos = tratando_erros_int("Quantos ciclos? ")
+    imag = tratando_erros_str("Deseja ativar os alertas de imagem? [S/N] ")
     if ciclos == 0:
         ciclos = 1
+    print(f"\n{'Pomodoro Iniciado':^53}\n")
 
 # Execução do Programa com Base Nas Informações Solicitadas.
     while manter_parametros == "S":
@@ -44,57 +45,71 @@ while True:
         while c <= ciclos:
             playmusic("BF.mp3")
             if ativo != 0:
-                print("Pomodoro ativo...")
+                print(f"Pomodoro ativo...")
+                hora_inicio_e_fim(ativo)
                 if imag == "S":
                     imagem(1, 7)  # fight
                 if ciclos != 1:
                     if ciclos == c:
                         print(f"Contagem de ciclos: {c}° e último ciclo.")
+                        print("\n")
                     else:
-                        print(f'Contagem de repetições: {c}º Ciclo.')
+                        print(f"Contagem de repetições: {c}º Ciclo.")
+                        print("\n")
                 else:
-                    print(f'Contagem de Ciclos: Ciclo único.')
+                    print(f"Contagem de Ciclos: Ciclo único.")
+                    print("\n")
                 sleep(ativo * 60)
-                playmusic('BF.mp3')
+                playmusic("BF.mp3")
 
             # Para o uso padrão de um Pomodoro (ativo + descanso)
             if descanso != 0 and ciclos != c and ciclos != 1 and ativo != 0:
-                print('Pomodoro para descanso...')
-                if imag == 'S':
+                print(f"Pomodoro para descanso... ")
+                hora_inicio_e_fim(descanso)
+                if imag == "S":
                     imagem(8, 12)  # bonfire
                 if ciclos != 1:
                     if c + 1 == ciclos:
-                        print(f'Contagem de Repetições: Preparando para o {c + 1}° e último ciclo.')
+                        print(f"Contagem de Repetições: Preparando para o {c + 1}° e último ciclo.")
+                        print("\n")
                     else:
-                        print(f'Contagem de Repetições: Preparando para o {c + 1}° ciclo.')
+                        print(f"Contagem de Repetições: Preparando para o {c + 1}° ciclo.")
+                        print("\n")
                 else:
-                    print(f'Contagem de Ciclos: Ciclo único.')
+                    print(f"Contagem de Ciclos: Ciclo único.")
+                    print("\n")
                 if ciclos != c:
                     sleep(descanso * 60)
 
             # Caso o tenha apenas o tempo de descanso
             if descanso != 0 and ativo == 0:
-                print('Pomodoro para descanso...')
+                print(f"Pomodoro para descanso...")
+                hora_inicio_e_fim(descanso)
                 if ativo == 0:
                     if ciclos != 1:
                         if ciclos == c:
-                            print(f'Contagem de ciclos: {c}° e último ciclo.')
+                            print(f"Contagem de ciclos: {c}° e último ciclo.")
+                            print("\n")
                         else:
-                            print(f'Contagem de repetições: {c}º Ciclo.')
+                            print(f"Contagem de repetições: {c}º Ciclo.")
+                            print("\n")
                     else:
-                        print(f'Contagem de Ciclos: Ciclo único.')
+                        print(f"Contagem de Ciclos: Ciclo único.")
+                        print("\n")
                 sleep(descanso * 60)
             c += 1
-        playmusic('VG.mp3')
-        if imag == 'S':
-            im = Image.open(f'fim.jpg')  # Fim
+        playmusic("VG.mp3")
+        if imag == "S":
+            im = Image.open(f"fim.jpg")  # Fim
             im.show()
-        print('Pomodoro Concluído!')
+        print("Pomodoro Concluído!")
 
-# Solicitação de Informações para Encerramento / Repetição do Programa.
-        loop = tratando_erros_str(loop, 'Deseja iniciar novamente? [S/N] ')
-        if loop == 'S':
-            manter_parametros = tratando_erros_str(manter_parametros, 'Deseja manter os mesmos parâmetros? [S/N] ')
-    if loop == 'N':
+        # Solicitação de Informações para Encerramento / Repetição do Programa.
+        loop = tratando_erros_str("Deseja iniciar novamente? [S/N] ")
+        if loop == "S":
+            manter_parametros = tratando_erros_str("Deseja manter os mesmos parâmetros? [S/N] ")
+            print("\n")
+    if loop == "N":
         break
 print("Don't give up, skeleton!")
+sleep(0.5)
--- Python/Pomodoro Souls/defpomodoro.py
+++ Python/Pomodoro Souls/defpomodoro.py
@@ -1,6 +1,7 @@
 from pygame import mixer
 from random import randint
 from PIL import Image
+from time import localtime
 
 
 def playmusic(msg):
@@ -10,24 +11,28 @@ def playmusic(msg):
     mixer.music.set_volume(0.9)
 
 
-def tratando_erros_int(parametro, msg):
-    while parametro < 0:
+def tratando_erros_int(msg):
+    while True:
         try:
             parametro = int(input(msg))
             if parametro < 0:
                 print('Hm...')
+            else:
+                break
         except ValueError:
             print('Hm....')
     return parametro
 
 
-def tratando_erros_str(parametro, msg):
-    while parametro not in 'SN':
+def tratando_erros_str(msg):
+    while True:
         try:
             parametro = str(input(msg)).strip().upper()[0]
             if parametro not in 'SN':
                 print('Hm...')
-        except ValueError:
+            else:
+                break
+        except (ValueError, IndexError):
             print('Hm....')
     return parametro
 
@@ -36,3 +41,16 @@ def imagem(a, b):
     n = randint(a, b)
     im = Image.open(f'img{n}.jpg')
     im.show()
+
+
+def hora_inicio_e_fim(tempo):
+    horas = localtime().tm_hour
+    minutos = localtime().tm_min
+    print(f"Iniciado às {horas:0>2}:{minutos:0>2}")
+    if minutos + tempo >= 60:
+        horas += (minutos + tempo) // 60
+        minutos = (minutos + tempo) % 60
+    else:
+        minutos += tempo
+    print(f"Encerra às {horas % 24:0>2}:{minutos:0>2}")
+
